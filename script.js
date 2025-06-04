63dec391478ab54553855b97159897eaf50abc10
Ignacio Molina <molinagranda@gmail.com>
2025-06-04 18:55:32 -0300

    BuscadorDePeliculas

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('searchButton').addEventListener('click', searchMovies); 
+
+function searchMovies() {
+    let searchInput = document.getElementById('searchInput').value;
+}
